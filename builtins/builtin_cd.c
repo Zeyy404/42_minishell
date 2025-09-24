f7d9269a6bb35b28decf7468dc0f5cd6d403d4ef
Zainab Salih <zainab.salih@outlook.com>
2025-09-24 21:48:58 +0400

    builtin_cd: fixed

--- builtins/builtin_cd.c
+++ builtins/builtin_cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_cd.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yalkhidi <yalkhidi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: zsalih <zsalih@student.42abudhabi.ae>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 15:50:23 by zsalih            #+#    #+#             */
-/*   Updated: 2025/09/23 11:12:44 by yalkhidi         ###   ########.fr       */
+/*   Updated: 2025/09/24 21:48:30 by zsalih           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,21 +37,54 @@ static int	get_target(char **target, char **argv, t_env **env)
 	return (0);
 }
 
-static void	update_pwd(t_env **env, char *old_pwd)
+static void	set_oldpwd(t_env **env, char *old_pwd)
 {
-	char	*new_pwd;
+	char	*pwd;
 
 	if (old_pwd)
 	{
 		env_set(env, "OLDPWD", old_pwd);
 		free(old_pwd);
 	}
+	else
+	{
+		pwd = env_get(*env, "PWD");
+		if (pwd)
+			env_set(env, "OLDPWD", pwd);
+	}
+}
+
+static void	set_newpwd(t_env **env, char *target)
+{
+	char	*new_pwd;
+	char	*pwd;
+	char	*tmp;
+
 	new_pwd = getcwd(NULL, 0);
 	if (new_pwd)
 	{
 		env_set(env, "PWD", new_pwd);
 		free(new_pwd);
+		return ;
 	}
+	pwd = env_get(*env, "PWD");
+	if (!pwd)
+		return ;
+	if (target[0] == '/')
+		env_set(env, "PWD", target);
+	else
+	{
+		tmp = concat(pwd, '/', target);
+		env_set(env, "PWD", tmp);
+		free(tmp);
+	}
+}
+
+static void	print_getcwd_error(void)
+{
+	ft_putstr_fd("cd: error retrieving current directory: getcwd: cannot"
+		"access parent directories: ", 2);
+	ft_putendl_fd(strerror(errno), 2);
 }
 
 int	builtin_cd(char **argv, t_shell *shell)
@@ -62,6 +95,8 @@ int	builtin_cd(char **argv, t_shell *shell)
 	if (!argv)
 		return (1);
 	buf = getcwd(NULL, 0);
+	if (!buf)
+		print_getcwd_error();
 	target = NULL;
 	if (get_target(&target, argv, &shell->env) != 0)
 		return (free(buf), 1);
@@ -69,10 +104,11 @@ int	builtin_cd(char **argv, t_shell *shell)
 	{
 		ft_putstr_fd("cd: ", 2);
 		ft_putstr_fd(target, 2);
-		ft_putendl_fd(": No such file or directory", 2);
+		perror(" ");
 		free(buf);
 		return (1);
 	}
-	update_pwd(&shell->env, buf);
+	set_oldpwd(&shell->env, buf);
+	set_newpwd(&shell->env, target);
 	return (0);
 }
