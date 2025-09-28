d0723162e60e7c56e82a1e196a874663334de433
Zainab Salih <zainab.salih@outlook.com>
2025-09-28 23:42:00 +0400

    update

--- tokenizer/token_syntax.c
+++ tokenizer/token_syntax.c
@@ -6,32 +6,13 @@
 /*   By: zsalih <zsalih@student.42abudhabi.ae>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 16:27:00 by zsalih            #+#    #+#             */
-/*   Updated: 2025/09/22 01:47:26 by zsalih           ###   ########.fr       */
+/*   Updated: 2025/09/28 23:29:26 by zsalih           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static int	check_start_token(t_token *tok)
-{
-	if (is_logic_op(tok->type))
-	{
-		printf("syntax error near unexpected token `%s'\n", tok->value);
-		return (258);
-	}
-	if (is_redir(tok->type) && (!tok->next || is_operator(tok->next->type)))
-	{
-		if (!tok->next)
-			printf("syntax error near unexpected token `newline'\n");
-		else
-			printf("syntax error near unexpected token `%s'\n",
-				tok->next->value);
-		return (258);
-	}
-	return (0);
-}
-
-static int	check_after_logic_op(t_token *tok)
+static int	check_token(t_token *tok)
 {
 	if (is_logic_op(tok->type))
 	{
@@ -79,10 +60,10 @@ int	check_syntax(t_token *tokens)
 	{
 		if (!prev)
 		{
-			if (check_start_token(tokens))
+			if (check_token(tokens))
 				return (258);
 		}
-		else if (is_logic_op(prev->type) && check_after_logic_op(tokens))
+		else if (is_logic_op(prev->type) && check_token(tokens))
 			return (258);
 		else if (is_redir(prev->type) && check_after_redir(tokens))
 			return (258);
